97a00db6089e6fb30a9c86e49a7134a4cd1aad24
OldMiller06 <dedal006@yandex.ru>
2025-12-09 03:11:18 +0300

    Добавлены unittest'ы

--- circle.py
+++ circle.py
@@ -1,5 +1,4 @@
-import math
-
+import math, unittest
 
 def area(r):
     '''
@@ -27,3 +26,30 @@ def perimeter(r):
 
     return 2 * math.pi * r
 
+class TestCircle(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(area(1), math.pi)
+        self.assertAlmostEqual(area(0), 0)
+        self.assertNotEqual(area(2), 10)
+        self.assertAlmostEqual(area(3), 28.274333882308138)
+        self.assertAlmostEqual(area(1.5), 7.0685834705770345)
+        self.assertAlmostEqual(area(2.5), 19.634954084936208)
+        self.assertNotEqual(area(4), 50)
+        self.assertAlmostEqual(area(0.5), 0.7853981633974483)
+        self.assertAlmostEqual(area(5), 78.53981633974483)
+        self.assertNotEqual(area(3), 30)
+
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(1), 2 * math.pi)
+        self.assertAlmostEqual(perimeter(0), 0)
+        self.assertNotEqual(perimeter(2), 10)
+        self.assertAlmostEqual(perimeter(3), 18.84955592153876)
+        self.assertAlmostEqual(perimeter(1.5), 9.42477796076938)
+        self.assertAlmostEqual(perimeter(2.5), 15.707963267948966)
+        self.assertNotEqual(perimeter(4), 20)
+        self.assertAlmostEqual(perimeter(0.5), 3.141592653589793)
+        self.assertAlmostEqual(perimeter(5), 31.41592653589793)
+        self.assertNotEqual(perimeter(3), 20)
+
+if __name__ == "__main__":
+    unittest.main()
--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,4 @@
+import unittest
 def area(a, b):
     '''
     Принимает две перпендикулярные стороны прямоугольника и возвращает его площадь
@@ -11,7 +12,7 @@ def area(a, b):
     '''
     return a * b
 
-def perimeter(a, b:)
+def perimeter(a, b):
     '''
     Принимает две перпендикулярные стороны прямоугольника и возвращает его периметр
 
@@ -24,3 +25,33 @@ def perimeter(a, b:)
     '''
 
     return 2*a + 2*b
+
+class TestRectangleFunctions(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(area(3, 4), 12)
+        self.assertEqual(area(0, 5), 0)
+        self.assertEqual(area(0, 0), 0)
+        self.assertAlmostEqual(area(2.5, 4.2), 10.5)
+        self.assertEqual(area(1000, 2000), 2000000)
+        self.assertEqual(area(-2, 5), -10)
+        self.assertEqual(area(-3, -4), 12)
+        self.assertAlmostEqual(area(0.1, 0.2), 0.02)
+        self.assertEqual(area(3, 7), area(7, 3))
+        self.assertAlmostEqual(area(14, 8), 112.0)
+
+
+    def test_perimeter(self):
+        self.assertEqual(perimeter(3, 4), 14)
+        self.assertEqual(perimeter(0, 5), 10)
+        self.assertEqual(perimeter(0, 0), 0)
+        self.assertAlmostEqual(perimeter(2.5, 4.2), 13.4)
+        self.assertEqual(perimeter(1000, 2000), 6000)
+        self.assertEqual(perimeter(-2, 5), 6)
+        self.assertEqual(perimeter(-3, -4), -14)
+        self.assertAlmostEqual(perimeter(0.1, 0.2), 0.6)
+        self.assertLess(perimeter(3, 4), perimeter(4, 4))
+        self.assertAlmostEqual(perimeter(14, 8), 44.0)
+
+if __name__ == "__main__":
+    unittest.main()
